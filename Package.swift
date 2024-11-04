d5e7dc7cd536b433838e05dcbcbca296d756adca
uday <udaygajera116@gmail.com>
2024-11-04 15:36:14 +0530

    v2.1.7 dependencies added

--- Package.swift
+++ Package.swift
@@ -6,19 +6,22 @@ import PackageDescription
 let package = Package(
     name: "VideoSDKRTCSwift",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "VideoSDKRTCSwift",
-            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup"]),
+            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup", "SocketIO", "Starscream"]),
     ],
-    dependencies: [],
+   dependencies: [],
     targets: [
         .target(name: "VideoSDKRTCSwift", dependencies: []),
         .binaryTarget(name: "WebRTC", path: "Sources/WebRTC.xcframework"),
         .binaryTarget(name: "Mediasoup", path: "Sources/Mediasoup.xcframework"),
         .binaryTarget(name: "VideoSDKRTC", path: "Sources/VideoSDKRTC.xcframework"),
+        .binaryTarget(name: "SocketIO", path: "Sources/SocketIO.xcframework"),
+        .binaryTarget(name: "Starscream", path: "Sources/Starscream.xcframework"),
         .testTarget(name: "VideoSDKRTCSwiftTests", dependencies: ["VideoSDKRTCSwift"]),
     ]
 )
+
---
+++ Sources/VideoSDKRTCSwift/VideoSDKRTCSwift.swift
@@ -0,0 +1,6 @@
+public struct VideoSDKRTCSwift {
+    public private(set) var text = "Hello World"
+
+    public init() {
+    }
+}
